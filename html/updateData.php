9551ddd9c63ae2392da6ed9972535205b26cb33a
Philipp Trenz <mail@philipptrenz.de>
2018-09-02 18:47:22 +0200

    Update updateData.php

--- html/updateData.php
+++ html/updateData.php
@@ -4,17 +4,17 @@
 	$co2mult = 0.7;
 
 
-	//Connection to database
+	// Connection to database
 	$db = new SQLite3("./SBFspot.db");
 
-	//Uptime
+	// Uptime
 	$query = "SELECT * FROM SpotData;";
 	$rs = $db->query($query);
 	$row = $rs->fetchArray(SQLITE3_ASSOC);
 	$uptime = $row['OperatingTime'];
 
 
-	//Huidig kwh van de laatste dag
+	// Daily yield of yesterday
 	$query = "SELECT TimeStamp, Power FROM DayData WHERE TimeStamp >= (SELECT (MAX(TimeStamp)-86400) FROM DayData);";
 	$rs = $db->query($query);
 	$row = $rs->fetchArray(SQLITE3_ASSOC);	
@@ -30,13 +30,13 @@
 		$i++; 
     } 
 
-	//Totaal van deze dag
+	// Total yield of today
 	$query = "SELECT EToday FROM SpotData WHERE TimeStamp = (SELECT MAX(TimeStamp) FROM SpotData);";
 	$rs = $db->query($query);
 	$row = $rs->fetchArray(SQLITE3_ASSOC);	
 	$totalday = $row['EToday'];
 
-	//Huidig totaal en CO2
+	// Latest yield and CO2 savings
 	$query = "SELECT ETotal FROM SpotData WHERE TimeStamp = (SELECT MAX(TimeStamp) FROM SpotData);";
 	$rs = $db->query($query);
 	$row = $rs->fetchArray(SQLITE3_ASSOC);
@@ -45,7 +45,7 @@
 	$co2 = round($co2) / 1000;
 	$totalall = round($total);
 
-	//Laatste update
+	// Last update
 	$query = "SELECT MAX(TimeStamp) AS TimeStamp FROM SpotData;";
 	$rs = $db->query($query);
 	$row = $rs->fetchArray(SQLITE3_ASSOC);
@@ -53,7 +53,7 @@
 
 
 
-	//Reset database connection
+	// Reset database connection
 	$db = NULL;
 
 	echo json_encode(array("last24h" => $table, "dayTotal" => $totalday, "total" => $totalall, "co2" => $co2, "update" => $epoch));
