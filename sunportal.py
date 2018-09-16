79fb12508e60f86f3f15ff6a70052ee55cd4b452
Philipp Trenz <mail@philipptrenz.de>
2018-09-16 21:06:32 +0200

    minor improvements

--- sunportal.py
+++ sunportal.py
@@ -31,4 +31,7 @@ def update():
 ###############################################
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=80)
+	try:
+		app.run(host='0.0.0.0', port=80)
+	except:
+		db.close()
