63ca8cf235e1effb0ba91a7c77d1ea5bc21d6774
Philipp Trenz <mail@philipptrenz.de>
2018-09-30 12:08:06 +0200

    fixed chart.js bug for stacked charts in month chart added automatic color generation for multiple inverters

--- static/js/main.js
+++ static/js/main.js
@@ -67,7 +67,7 @@ function initializeCanvas() {
                 <div class="chart-container">
                     <i class="fa fa-chevron-left navigation-chevrons navigation-left" onclick="navigate(this)" style="visibility: hidden;"></i>
                     <div class="chart-container-inner">
-                        <canvas id='`+`chart-day' class='chart-canvas' height='200px'/>
+                        <canvas id='`+`chart-day' class='chart-canvas' height='300px'/>
                     </div>
                     <i class="fa fa-chevron-right navigation-chevrons navigation-right" onclick="navigate(this)" style="visibility: hidden;"></i>
                 </div>
@@ -92,7 +92,7 @@ function initializeCanvas() {
                 <div class="chart-container">
                     <i class="fa fa-chevron-left navigation-chevrons navigation-left" onclick="navigate(this)" style="visibility: hidden;"></i>
                     <div class="chart-container-inner">
-                        <canvas id='`+`chart-month' class='chart-canvas' height='200px'/>
+                        <canvas id='`+`chart-month' class='chart-canvas' height='300px'/>
                     </div>
                     <i class="fa fa-chevron-right navigation-chevrons navigation-right" onclick="navigate(this)" style="visibility: hidden;"></i>
                 </div>
@@ -303,16 +303,6 @@ function loadData(day) {
 	                moment.unix(all.day.interval.to)
 	            ];
 
-                /*
-	            all.day.data.forEach(function(obj) {
-	                obj.x = moment.unix(obj.time);
-	                obj.y = obj.power / 1000;
-	                delete obj.time;
-	                delete obj.power;
-	            })
-	            dayChart.data.datasets[0].data = all.day.data;
-                */
-
                 // update month chart
 	            $("#chart-month-col .chart-date").text(getMonthStringForPrint());
 	            $("#chart-month-col .inverter-yield").text( addPrefix(all.month.total) + "Wh");
@@ -331,15 +321,6 @@ function loadData(day) {
 	                moment.unix(all.month.interval.to)
 	            ];
 
-	            /*
-	            all.month.data.forEach(function(obj) {
-	                obj.x = moment.unix(obj.time).subtract(1, 'days');
-	                obj.y = obj.power / 1000;
-	                delete obj.time;
-	                delete obj.power;
-	            })
-	            monthChart.data.datasets[0].data = all.month.data;
-                */
 	        }
 
 	        var chart_num = 0;
@@ -350,7 +331,10 @@ function loadData(day) {
 
                     {
 
-                        // WORKAROUND FOR CHART.JS BUG
+                        // WORKAROUND FOR CHART.JS STACKED CHARTS BUG
+                        // ISSUE: https://github.com/chartjs/Chart.js/issues/5484
+
+                        // for day data
                         if (all.day.data[0].time < inv_data.day.data[0].time) {
                             var tmp = [], i = 0;
                             while (all.day.data[i].time < inv_data.day.data[0].time) {
@@ -360,18 +344,18 @@ function loadData(day) {
                             var tmp2 = tmp.concat(inv_data.day.data);
                             inv_data.day.data = tmp2;
                         }
-                        if (all.day.data[all.day.data.length-1].time > inv_data.day.data[inv_data.day.data.length-1].time) {
-                            var tmp, i = all.day.data.length-1;
-                            while (all.day.data[i].time < inv_data.day.data[inv_data.day.data.length-1].time) {
-                                tmp.unshift( { 'time': all.day.data[i].time, 'power': 0 } );
-                                i--;
+
+                        // for month data
+                        if (all.month.data[0].time < inv_data.month.data[0].time) {
+                            var tmp = [], i = 0;
+                            while (all.month.data[i].time < inv_data.month.data[0].time) {
+                                tmp.push( { 'time': all.month.data[i].time, 'power': 0 } );
+                                i++;
                             }
-                            inv_data.day.data.concat(tmp);
+                            var tmp2 = tmp.concat(inv_data.month.data);
+                            inv_data.month.data = tmp2;
                         }
                         // WORKAROUND FOR CHART.JS BUG END
-
-
-
 
 
                         // update day chart
@@ -386,8 +370,6 @@ function loadData(day) {
                             dayChart.data.datasets[chart_num] = {
                                 label: response.today.inverters[k].name,
                                 data: inv_data.day.data,
-                                backgroundColor: 'rgba(227,6,19,0.3)',
-                                borderColor: 'rgba(227,6,19,1)',
                                 borderWidth: 1,
                                 pointRadius: 0
                             }
@@ -411,23 +393,38 @@ function loadData(day) {
                             monthChart.data.datasets[chart_num] = {
                                 label: response.today.inverters[k].name,
                                 data: inv_data.month.data,
-                                backgroundColor: 'rgba(227,6,19,0.3)',
-                                borderColor: 'rgba(227,6,19,1)',
                                 borderWidth: 1,
                                 pointRadius: 0
                             }
                         } else {
                             monthChart.data.datasets[chart_num].data = inv_data.month.data;
                         }
+                        console.log()
                     }
                     chart_num++;
                 }
             }
 
+
+
+
+            var backgroundColorShades   = getColorShades(dayChart.data.datasets.length, 'rgba(227,6,19,0.3)')
+            var borderColorShades       = getColorShades(dayChart.data.datasets.length, 'rgba(227,6,19,1)')
+
             dayChart.data.datasets.sort(sort_inverter_datasets_alphabetically);   // sort alphabetically
+            for (var i=0; i<dayChart.data.datasets.length; i++) {
+                obj = dayChart.data.datasets[i];
+                obj.backgroundColor = backgroundColorShades[i];
+                obj.borderColor = borderColorShades[i];
+            }
             dayChart.update();
 
             monthChart.data.datasets.sort(sort_inverter_datasets_alphabetically); // sort alphabetically
+            for (var i=0; i<monthChart.data.datasets.length; i++) {
+                obj = monthChart.data.datasets[i];
+                obj.backgroundColor = backgroundColorShades[i];
+                obj.borderColor = borderColorShades[i];
+            }
             monthChart.update();
 
 			requesting = false;
@@ -518,4 +515,35 @@ function getMonthStringForPrint() {
 	} else {
 		return '';
 	}
+}
+
+function getColorShades(num, color) {
+    var shades = Array();
+    var min = 0.5, max = -0.5
+    for (var i=1; i<= num; i++) {
+        var percentage = min+i*(max-min)/(num+1);
+        shades.push(shadeBlendConvert(percentage, color));
+    }
+    return shades;
+}
+
+const shadeBlendConvert = function (p, from, to) {
+    if(typeof(p)!="number"||p<-1||p>1||typeof(from)!="string"||(from[0]!='r'&&from[0]!='#')||(to&&typeof(to)!="string"))return null; //ErrorCheck
+    if(!this.sbcRip)this.sbcRip=(d)=>{
+        let l=d.length,RGB={};
+        if(l>9){
+            d=d.split(",");
+            if(d.length<3||d.length>4)return null;//ErrorCheck
+            RGB[0]=i(d[0].split("(")[1]),RGB[1]=i(d[1]),RGB[2]=i(d[2]),RGB[3]=d[3]?parseFloat(d[3]):-1;
+        }else{
+            if(l==8||l==6||l<4)return null; //ErrorCheck
+            if(l<6)d="#"+d[1]+d[1]+d[2]+d[2]+d[3]+d[3]+(l>4?d[4]+""+d[4]:""); //3 or 4 digit
+            d=i(d.slice(1),16),RGB[0]=d>>16&255,RGB[1]=d>>8&255,RGB[2]=d&255,RGB[3]=-1;
+            if(l==9||l==5)RGB[3]=r((RGB[2]/255)*10000)/10000,RGB[2]=RGB[1],RGB[1]=RGB[0],RGB[0]=d>>24&255;
+        }
+    return RGB;}
+    var i=parseInt,r=Math.round,h=from.length>9,h=typeof(to)=="string"?to.length>9?true:to=="c"?!h:false:h,b=p<0,p=b?p*-1:p,to=to&&to!="c"?to:b?"#000000":"#FFFFFF",f=this.sbcRip(from),t=this.sbcRip(to);
+    if(!f||!t)return null; //ErrorCheck
+    if(h)return "rgb"+(f[3]>-1||t[3]>-1?"a(":"(")+r((t[0]-f[0])*p+f[0])+","+r((t[1]-f[1])*p+f[1])+","+r((t[2]-f[2])*p+f[2])+(f[3]<0&&t[3]<0?")":","+(f[3]>-1&&t[3]>-1?r(((t[3]-f[3])*p+f[3])*10000)/10000:t[3]<0?f[3]:t[3])+")");
+    else return "#"+(0x100000000+r((t[0]-f[0])*p+f[0])*0x1000000+r((t[1]-f[1])*p+f[1])*0x10000+r((t[2]-f[2])*p+f[2])*0x100+(f[3]>-1&&t[3]>-1?r(((t[3]-f[3])*p+f[3])*255):t[3]>-1?r(t[3]*255):f[3]>-1?r(f[3]*255):255)).toString(16).slice(1,f[3]>-1||t[3]>-1?undefined:-2);
 }
