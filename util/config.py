f55b5aff6afe3cbf0c4fe90c7a46ede363e6460e
philipptrenz <mail@philipptrenz.de>
2018-11-22 20:39:14 +0100

    pylint travis-ci integration test

--- util/config.py
+++ util/config.py
@@ -19,7 +19,7 @@ class Config():
             try:
                 with open('config.yml') as f:
                     self.config = yaml.load(f)
-            except OSError:
+            except Exception:
                 with open('config.default.yml') as f:
                     self.config = yaml.load(f)
 
